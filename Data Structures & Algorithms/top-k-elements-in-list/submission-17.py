b0fb098b52e0eb44d8a43fa7fbafaf866f7e2cd0
Tian Feng <feng.txy@outlook.com>
2026-05-04 17:23:25 -0700

    Add: top-k-elements-in-list - submission-17

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-17.py
@@ -0,0 +1,22 @@
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        num_freq = dict()
+        for num in nums:
+            num_freq[num] = num_freq.get(num, 0) + 1 
+        
+        bucket = [[] for i in range(len(nums)+1)]
+
+        for num, freq in num_freq.items():
+            bucket[freq].append(num)
+        res = []
+        for i in range(len(nums), -1, -1):
+            if not bucket[i]:
+                continue
+            for num in bucket[i]:
+                res.append(num)
+            if len(res) == k:
+                break
+        return res 
+        
